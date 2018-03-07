a0b29570af5cef28d4d8a22fcd215ae361b4e04f
Kay Mok <kaymok3@gmail.com>
2018-03-07 14:01:31 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+// Javascript Functions & Scope
+
+
+// Function with no arguments
+// Declare a function called bestTeacher that takes no arguments and logs to the console “{Your Teacher} is the best teacher in the world” to the console. ;)
+
+
+// Function with one argument
+// NYCDA needs a simple welcome program for registered students.
+// Create a function called nycdaWelcome that will take in a student's name as a function argument and console log a string that looks like : “Welcome to NYCDA {Students Name}. Enjoy your stay”
+
+
+// Function with multiple arguments
+// Create a simple calculator function called simpleCalc that given two arguments will perform addition and return the result.
+
+
+// Function with arguments and logic
+// Declare a function called magicDoor that receives an input of 1, 2 or 3. Depending upon which virtual "door" was entered create logic that will tell user they've received a different "prize" in an alert based on their selection
+// Door 1 will give the user a brand new car
+// Door 2 will give the user a nycda t-shirt
+// Door 3 will turn into a blackhole and teleport you to antarctica
+
+
+// Function that modifies input
+// Define a function greaterOfThree that takes three integers and returns the larger of the three.
+
+
+// Function that modifies input
+// Define a function lineThemUp that takes three integers as arguments and returns the integers as an array.
+
+
+// Function that modifies input
+// Define a function called knighter that takes a name and returns it after being knighted.
+// ex : Knighter(“Andy”) returns “Royal Knight Sir Andy”
+
+
+// Function with Logic
+// Define a function called royalizer that takes a gender and name as arguments, and returns a title based on the gender.
+// ex : royalizer(“male”, “David”) returns “His Majesty David” , for female it would be “Her Majesty X”.
